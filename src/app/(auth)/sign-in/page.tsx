74ae543771b8f9f10dcbd0d7fcbdcd3b3b25e426
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-12-03 11:00:12 -0300

    feat: add back to home button in sign-in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,21 @@
 import { SignInForm } from "@/components/forms/sign-in-form";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 
 export default function SignInPage() {
   return (
-    <div className="flex flex-col items-center gap-6">
+    <div className="relative flex h-full w-full max-w-screen-xl flex-col items-center justify-center gap-8">
+      <Button
+        variant="ghost"
+        asChild
+        className="absolute top-10 left-10"
+      >
+        <Link href="/">
+          <ChevronLeft className="size-4" />
+          Back
+        </Link>
+      </Button>
       <header className="text-center">
         <h2 className="font-semibold text-3xl leading-normal tracking-tight">
           Welcome back! 👋
