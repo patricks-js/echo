ad9a8f2e4af74767a87619cf343eef66b44b86b1
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-12-09 14:04:16 -0300

    refactor: transfer auth control to hono

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,7 +14,6 @@ export async function middleware(req: NextRequest) {
 export const config = {
   matcher: [
     // Always run for API routes
-    "/(api|trpc)(.*)",
     "/editor(.*)",
     "/settings(.*)",
   ],
