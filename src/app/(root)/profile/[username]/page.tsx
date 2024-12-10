eac1db05a7762ce220df87f5a39cce4e53db3933
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-12-09 23:08:47 -0300

    feat: make blog card first look

--- src/app/(root)/profile/[username]/page.tsx
+++ src/app/(root)/profile/[username]/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { AccountSidebar } from "@/features/profile/components/account-sidebar";
 import { useGetProfile } from "@/features/profile/hooks/use-get-profile";
+import { Eye } from "lucide-react";
 import { use } from "react";
 
 type PageParams = {
@@ -21,7 +24,45 @@ export default function ProfilePage({ params }: PageParams) {
   return (
     <div className="grid grid-cols-[288px_1fr] gap-8">
       <AccountSidebar user={data} />
-      <h2 className="border text-3xl">Posts</h2>
+      <section className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+        {Array.from({ length: 7 }).map((_, a) => (
+          <div
+            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            key={a}
+            className="max-w-xs space-y-4 rounded-2xl border px-6 py-8 shadow-card"
+          >
+            <header className="flex items-center justify-between">
+              <time
+                dateTime="2024-12-09 00:00:00"
+                className="rounded-full bg-secondary px-3 py-2"
+              >
+                20 May, 2023
+              </time>
+              <span className="inline-flex items-center gap-1">
+                <Eye className="size-5" />
+                232
+              </span>
+            </header>
+            <div className="flex flex-col items-start gap-4">
+              <p className="line-clamp-2 text-muted-foreground">
+                Lorem ipsum dolor quaerat unde beatae architecto sapiente aut
+                modi quidem libero consectetur ullam quam! Laborum, adipisci
+                officia.
+              </p>
+              <img
+                src=""
+                alt="banner"
+                className="h-40 w-full rounded-lg border bg-secondary"
+              />
+            </div>
+            <footer className="flex items-center justify-between">
+              <Badge variant="secondary">EdTech</Badge>
+              {/* TODO: Push to post details */}
+              <Button>Details</Button>
+            </footer>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
